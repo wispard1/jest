48f8108e1b83c8ee375d43e14087c6b2f7398a3f
wispard1 <antonvlasov610@gmail.com>
2025-04-17 18:48:49 +0800

    add fixtures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,14 @@
 import reverse from '../src/index.js';
+import fs from 'fs';
 
-test('reverse', () => {
-  expect(reverse('hello')).toEqual('olleh');
-  expect(reverse('')).toEqual('');
+test('reverse furnction with long text', () => {
+  const inputPath = `${__dirname}/../__fixtures__/input.txt`;
+  const text = fs.readFileSync(inputPath, 'utf-8');
+
+  const outputPath = `${__dirname}/../__fixtures__/output.txt`;
+  const expectedResult = fs.readFileSync(outputPath, 'utf-8');
+
+  const result = reverse(text);
+
+  expect(result).toEqual(expectedResult);
 });
